019dc8041400dd75bed67d35bd84bdde0d533cab
Paul Jefferies <jefferie@umich.edu>
2015-06-20 21:26:41 -0400

    Corrected/clarified comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,15 @@
-## makeCacheMatrix and cashSolve allow the caching of the potentially
-## lengthy calculation of the inverse of a matrix. When a matrix
-## is first inverted, it is stored for later use. If it is called
-## to be inverted again, the cached version is returned. To accomplish
-## this, a dummy matrix is first created by makeCacheMatrix. Then
-## the matrix is either inverted or the cached version is returned
-## by cacheSolve
+## makeCacheMatrix and cashSolve allow the caching of the result of
+## the potentially lengthy calculation of the invertion of a matrix.
+## When a matrix is first inverted, it is stored for later use. If
+## it is called to be inverted again, the cached version is
+## returned. To accomplishthis, a dummy matrix (as a list) is first
+## created by makeCacheMatrix. Then the matrix is either inverted or
+## the cached version is returned by cacheSolve
 
 # makeCacheMatrix creates dummy matrix as a list of four functions
 # used by cacheSolve to check for, cache and retrieve matrix
 # inversions. makeCacheMatrix accepts a matrix as it's input and
-# assumes that it is invertible.
+# assumes that the matrix is invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
@@ -29,8 +29,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 #cachSolve takes a 'list-matrix' created by makeCacheMatrix, checks
 #to see if there is an inverse cached, returns it if there is,
-#inverts it using 'solve()' if it hasn't been cached, caches the
-#inverse if newly created and returns the new inverse.
+#inverts it using 'solve()' if the matrix inverse hasn't been cached,
+#caches the inverse if newly created and returns the new inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
